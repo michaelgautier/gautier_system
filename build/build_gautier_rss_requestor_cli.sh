2c01f2051a535dc3a9f2e79340da9a4aaaabee0f
Michael Gautier <michaelgautier.wordpress.com>
2017-08-26 05:14:49 -0500

    began feeds file parse

--- build/build_gautier_rss_requestor_cli.sh
+++ build/build_gautier_rss_requestor_cli.sh
@@ -5,6 +5,7 @@ reset
 
 g++ -std=c++14 -c -g -I../gautier_rss_requestor_lib/ -I/usr/include -lgautier_rss_requestor -o obj/gautier_rss_requestor_cli.o ../gautier_rss_requestor_lib/cli.cxx
 g++ -lPocoFoundation -lPocoUtil -lPocoNet -lPocoXML -o bin/gautier_rss_requestor_cli obj/gautier_rss_requestor_lib.o obj/gautier_rss_requestor_cli.o
+cp ../config/feeds.txt bin
 
 echo "	build"
 ls -gGhU
--- gautier_rss_requestor_lib/cli.cxx
+++ gautier_rss_requestor_lib/cli.cxx
@@ -2,12 +2,33 @@
 #include <gautier_rss_request.hxx>
 #include <gautier_rss_requestor.hxx>
 
+#include <iostream>
+#include <fstream>
+
 int main() {
         
 	gautier::system::rss::gautier_rss_requestor rss_requestor;
 	
 	gautier::system::rss::gautier_rss_request test_request;
 	
+         
+        std::ifstream feeds_file;
+        feeds_file.open("feeds.txt");
+        
+        std::string feeds_lines;
+        
+        while(!feeds_file.eof()) {
+                char feeds_data;
+                
+                feeds_data = feeds_file.get();
+                
+                feeds_lines.push_back(feeds_data);
+        }
+        
+        //to do: read the feeds data.       
+        
+        feeds_file.close();
+        
 	test_request.feed_name = "arstechnica";
 	test_request.feed_url = "http://feeds.arstechnica.com/arstechnica/index";
 
