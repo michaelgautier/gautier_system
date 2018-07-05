703852541c51a0fe03736ba33ec5d617e9210c06
Michael Gautier <michaelgautier.wordpress.com>
2018-07-05 00:57:31 -0500

    Changed user-agent

--- lib/news/techconstruct/http.cxx
+++ lib/news/techconstruct/http.cxx
@@ -41,7 +41,7 @@ void cls::get_stream(string url, string& output) {
     HTTPClientSession http_session(request_uri.getHost(), request_uri.getPort());
 
     HTTPRequest http_request(request_method, url);
-    http_request.set("User-Agent", "Googlebot/2.1 (+http://www.google.com/bot.html)");
+    http_request.set("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
 
     try {
         http_session.sendRequest(http_request);
