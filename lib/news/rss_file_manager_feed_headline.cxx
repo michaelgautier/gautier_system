374024c05e509b8461d468c617ffa6f293fb0741
Michael Gautier <michaelgautier.wordpress.com>
2018-07-14 01:12:56 -0500

    Time validation on empty file

--- lib/news/rss_file_manager_feed_headline.cxx
+++ lib/news/rss_file_manager_feed_headline.cxx
@@ -79,29 +79,33 @@ bool cls::get_can_feed_refresh(const news::rss_data_feed_name_spec& feed_name) {
         }
     };
 
-    rss_techconstruct::file rssfile;
-    rssfile.read_file_into_string(_file_location, call_rss_line);
+    if(!last_checked_date_time_string.empty()) {
+        rss_techconstruct::file rssfile;
+        rssfile.read_file_into_string(_file_location, call_rss_line);
 
-    auto last_checked_date_time_seconds_l = stoll(last_checked_date_time_string);
+        auto last_checked_date_time_seconds_l = stoll(last_checked_date_time_string);
 
-    auto last_checked_date_time_seconds_t = (time_t)last_checked_date_time_seconds_l;
+        auto last_checked_date_time_seconds_t = (time_t)last_checked_date_time_seconds_l;
 
-    auto last_checked_date_time = ctime(&last_checked_date_time_seconds_t);
+        auto last_checked_date_time = ctime(&last_checked_date_time_seconds_t);
 
-    auto current_time_t = time(NULL);
+        auto current_time_t = time(NULL);
 
-    auto current_time = ctime(&current_time_t);
+        auto current_time = ctime(&current_time_t);
 
-    auto elapsed_seconds = difftime(current_time_t, last_checked_date_time_seconds_t);
+        auto elapsed_seconds = difftime(current_time_t, last_checked_date_time_seconds_t);
 
-    cout
-            << feed_name.name << "\n"
-            << "\tLast checked: " << last_checked_date_time << " "
-            << "\tCurrent time: " << current_time << " "
-            << "\tSeconds since last update: " << elapsed_seconds << " \n";
+        cout
+                << feed_name.name << "\n"
+                << "\tLast checked: " << last_checked_date_time << " "
+                << "\tCurrent time: " << current_time << " "
+                << "\tSeconds since last update: " << elapsed_seconds << " \n";
 
-    //Only refresh rss feed no earlier than once an hour = 3,600 seconds.
-    can_refresh = elapsed_seconds > 3599;
+        //Only refresh rss feed no earlier than once an hour = 3,600 seconds.
+        can_refresh = elapsed_seconds > 3599;
+    } else {
+        can_refresh = true;
+    }
 
     return can_refresh;
 }
