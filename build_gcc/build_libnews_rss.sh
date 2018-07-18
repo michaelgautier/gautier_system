da8d7326435ed5f0358015833c63564cd4c350c9
Michael Gautier <michaelgautier.wordpress.com>
2018-07-18 01:04:59 -0500

    Set proper dependency order

--- build_gcc/build_libnews_rss.sh
+++ build_gcc/build_libnews_rss.sh
@@ -6,12 +6,19 @@
 #Portions of the POCO C++ Libraries utilize the following copyrighted material, the use of which is hereby acknowledged.
 #POCO C++ Libraries released under the Boost Software License; Copyright 2018, Applied Informatics Software Engineering GmbH and Contributors; 
 #C++ Standard Library; Copyright 2018 Standard C++ Foundation.
+
 echo "Building rss_requestor library"
+
 ./build_libnews_file.sh
 ./build_libnews_http.sh
 
 echo "build rss lib"
+
 rm obj/libnews_rss*.o
+
+#Application Bridge
+g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_data_consequence_spec.o ../lib/news/rss_data_consequence_spec.cxx
+g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_set_consequence.o ../lib/news/rss_set_consequence.cxx
 
 #Feed Data
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_data_feed_name_spec.o ../lib/news/rss_data_feed_name_spec.cxx
@@ -23,6 +30,7 @@ g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_set_feed_headline.o
 
 #File Managers
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_file_manager_feed_name.o ../lib/news/rss_file_manager_feed_name.cxx
+
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -I../lib/news/techconstruct/ -o obj/libnews_rss_file_manager_feed_headline.o ../lib/news/rss_file_manager_feed_headline.cxx
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -I../lib/news/techconstruct/ -o obj/libnews_rss_file_manager_feed_article.o ../lib/news/rss_file_manager_feed_article.cxx
 
@@ -30,10 +38,6 @@ g++ -std=c++14 -c -fPIC -g -I../lib/news/ -I../lib/news/techconstruct/ -o obj/li
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_cycle_feed_name.o ../lib/news/rss_cycle_feed_name.cxx
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_cycle_feed_headline.o ../lib/news/rss_cycle_feed_headline.cxx
 g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_cycle_feed_article.o ../lib/news/rss_cycle_feed_article.cxx
-
-#Application Bridge
-g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_data_consequence_spec.o ../lib/news/rss_data_consequence_spec.cxx
-g++ -std=c++14 -c -fPIC -g -I../lib/news/ -o obj/libnews_rss_set_consequence.o ../lib/news/rss_set_consequence.cxx
 
 ls -gGh obj/libnews_rss*
 
